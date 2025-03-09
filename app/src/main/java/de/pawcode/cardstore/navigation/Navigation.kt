bad58a134879c98f903035e433b5c0d98298d7fb
pawcode Development <development@pawcode.de>
2025-03-09 16:27:14 +0100

    refactor(card-details): remove unused screen

--- app/src/main/java/de/pawcode/cardstore/navigation/Navigation.kt
+++ app/src/main/java/de/pawcode/cardstore/navigation/Navigation.kt
@@ -27,7 +27,6 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import de.pawcode.cardstore.data.services.SnackbarService
 import de.pawcode.cardstore.ui.screens.AddEditCardScreen
-import de.pawcode.cardstore.ui.screens.CardDetailScreen
 import de.pawcode.cardstore.ui.screens.CardListScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -56,13 +55,6 @@ fun Navigation() {
         var currentRoute = currentDestination(navController)
         when (currentRoute?.route) {
             Screen.CardList.route -> AppBar(title = "Card Store")
-            Screen.CardDetail.route + "/{cardId}" -> AppBar(
-                title = "Card Detail", navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                })
-
             Screen.AddEditCard.route + "?cardId={cardId}" -> AppBar(
                 title = "Add/Edit Card", navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
@@ -92,13 +84,6 @@ fun Navigation() {
                 CardListScreen(navController = navController)
             }
             composable(
-                route = Screen.CardDetail.route + "/{cardId}",
-                arguments = listOf(navArgument("cardId") { type = NavType.StringType })
-            ) { entry ->
-                val cardId = entry.arguments?.getString("cardId") ?: ""
-                CardDetailScreen(navController = navController, cardId = cardId)
-            }
-            composable(
                 route = Screen.AddEditCard.route + "?cardId={cardId}",
                 arguments = listOf(navArgument("cardId") {
                     type = NavType.StringType
@@ -120,7 +105,6 @@ fun currentDestination(navController: NavController): NavDestination? {
 
 sealed class Screen(val route: String) {
     object CardList : Screen("card_list")
-    object CardDetail : Screen("card_detail")
     object AddEditCard : Screen("add_edit_card")
 }
 
