269a0889fe392dc7cf1a4a1d9a5fdf3587c52cb8
pawcode Development <development@pawcode.de>
2025-03-14 21:36:23 +0100

    feat(cards-list): show hint to add first card

--- app/src/main/java/de/pawcode/cardstore/ui/components/CardsListComponent.kt
+++ app/src/main/java/de/pawcode/cardstore/ui/components/CardsListComponent.kt
@@ -1,13 +1,20 @@
 package de.pawcode.cardstore.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.pawcode.cardstore.data.database.entities.CardEntity
@@ -22,20 +29,34 @@ fun CardsListComponent(
     onCardClicked: (CardEntity) -> Unit,
     onCardLongPressed: (CardEntity) -> Unit,
 ) {
-    LazyVerticalGrid(
-        state = listState,
-        columns = GridCells.Adaptive(minSize = 300.dp),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(
-            items = cards, key = { card -> card.id }) { card ->
-            CardComponent(card = card, onClick = {
-                onCardClicked(card)
-            }, onLongPress = {
-                onCardLongPressed(card)
-            })
+    if (cards.isEmpty()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Create your first card",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    } else {
+        LazyVerticalGrid(
+            state = listState,
+            columns = GridCells.Adaptive(minSize = 300.dp),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(
+                items = cards, key = { card -> card.id }) { card ->
+                CardComponent(card = card, onClick = {
+                    onCardClicked(card)
+                }, onLongPress = {
+                    onCardLongPressed(card)
+                })
+            }
         }
     }
 }
@@ -49,5 +70,20 @@ fun PreviewCardsListComponent() {
             EXAMPLE_CARD,
             EXAMPLE_CARD.copy(id = Uuid.random().toString()),
             EXAMPLE_CARD.copy(id = Uuid.random().toString())
-        ), listState = rememberLazyGridState(), onCardClicked = {}, onCardLongPressed = {})
+        ),
+        listState = rememberLazyGridState(),
+        onCardClicked = {},
+        onCardLongPressed = {}
+    )
+}
+
+@Preview
+@Composable
+fun PreviewCardsListComponentEmpty() {
+    CardsListComponent(
+        cards = listOf(),
+        listState = rememberLazyGridState(),
+        onCardClicked = {},
+        onCardLongPressed = {}
+    )
 }
