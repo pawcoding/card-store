12309e37d4be3d63caa691e4d4e68c9367e598a7
pawcode Development <development@pawcode.de>
2025-03-30 15:03:26 +0200

    feat(color-picker): add hex-code input to specify exact color

--- app/src/main/java/de/pawcode/cardstore/ui/dialogs/ColorPickerDialog.kt
+++ app/src/main/java/de/pawcode/cardstore/ui/dialogs/ColorPickerDialog.kt
@@ -4,43 +4,92 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.core.graphics.toColorInt
 import com.github.skydoves.colorpicker.compose.BrightnessSlider
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import de.pawcode.cardstore.R
 
+@OptIn(ExperimentalStdlibApi::class)
 @Composable
 fun ColorPickerDialog(color: Color, onDismiss: (Color?) -> Unit) {
     val controller = rememberColorPickerController()
 
+    var hexValue by remember {
+        mutableStateOf(
+            controller.selectedColor.value.toArgb().toHexString(HexFormat.UpperCase).drop(2)
+        )
+    }
+
+    fun onColorInput(input: String) {
+        var newValue = input
+            .uppercase()
+            .filter { it.isDigit() || it in 'A'..'F' }
+            .take(6)
+
+        hexValue = newValue
+
+        if (newValue.length != 6) {
+            return
+        }
+
+        try {
+            val color = Color(("#$newValue").toColorInt())
+            controller.selectByColor(color, true)
+        } catch (_: IllegalArgumentException) {
+        }
+    }
+
+    LaunchedEffect(controller.selectedColor.value) {
+        hexValue = controller.selectedColor.value.toArgb().toHexString(HexFormat.UpperCase).drop(2)
+    }
+
     Dialog(
-        onDismissRequest = { onDismiss(null) }
+        onDismissRequest = { onDismiss(null) },
+        properties = DialogProperties(
+            dismissOnClickOutside = false
+        )
     ) {
         Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp), shape = MaterialTheme.shapes.large
+            modifier = Modifier.fillMaxWidth(),
+            shape = MaterialTheme.shapes.large
         ) {
             Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(16.dp)
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(16.dp)
             ) {
                 Text(
                     text = stringResource(R.string.card_pick_color),
@@ -50,17 +99,36 @@ fun ColorPickerDialog(color: Color, onDismiss: (Color?) -> Unit) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
                     Box(
                         modifier = Modifier
-                            .size(44.dp)
+                            .height(IntrinsicSize.Min)
+                            .size(56.dp)
                             .clip(
                                 MaterialTheme.shapes.small
                             )
                             .background(controller.selectedColor.value),
-                        contentAlignment = Alignment.Center
                     ) {}
+
+                    OutlinedTextField(
+                        value = hexValue,
+                        onValueChange = { onColorInput(it) },
+                        placeholder = { Text(stringResource(R.string.card_color_hex)) },
+                        prefix = {
+                            Text(text = "#", fontFamily = FontFamily.Monospace)
+                        },
+                        textStyle = TextStyle(
+                            fontFamily = FontFamily.Monospace
+                        ),
+                        modifier = Modifier.fillMaxWidth(),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            capitalization = KeyboardCapitalization.Characters,
+                            imeAction = ImeAction.Done
+                        )
+                    )
                 }
 
                 HsvColorPicker(
