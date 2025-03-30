72b064741e264ccf83c3639840b57fb26503ff0f
pawcode Development <development@pawcode.de>
2025-03-30 09:43:54 +0200

    feat(color-picker): show preview of selected color

--- app/src/main/java/de/pawcode/cardstore/ui/dialogs/ColorPickerDialog.kt
+++ app/src/main/java/de/pawcode/cardstore/ui/dialogs/ColorPickerDialog.kt
@@ -1,17 +1,22 @@
 package de.pawcode.cardstore.ui.dialogs
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,6 +46,22 @@ fun ColorPickerDialog(color: Color, onDismiss: (Color?) -> Unit) {
                     text = stringResource(R.string.card_pick_color),
                     style = MaterialTheme.typography.headlineSmall,
                 )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(44.dp)
+                            .clip(
+                                MaterialTheme.shapes.small
+                            )
+                            .background(controller.selectedColor.value),
+                        contentAlignment = Alignment.Center
+                    ) {}
+                }
 
                 HsvColorPicker(
                     modifier = Modifier
