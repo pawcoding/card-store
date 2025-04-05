525e8861db60a0ee56d0f98039b916ea6957a867
pawcode Development <development@pawcode.de>
2025-04-05 19:36:56 +0200

    fix(card-scanner): copy error to clipboard

--- app/src/main/java/de/pawcode/cardstore/ui/utils/BarcodeScanner.kt
+++ app/src/main/java/de/pawcode/cardstore/ui/utils/BarcodeScanner.kt
@@ -1,10 +1,17 @@
 package de.pawcode.cardstore.ui.utils
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+import de.pawcode.cardstore.R
+import de.pawcode.cardstore.data.services.SnackbarService
 
 @Composable
 fun BarcodeScanner(onBarcodeDetected: (Barcode) -> Unit, onCancel: () -> Unit) {
@@ -18,9 +25,31 @@ fun BarcodeScanner(onBarcodeDetected: (Barcode) -> Unit, onCancel: () -> Unit) {
 
   val scanner = GmsBarcodeScanning.getClient(context, options)
 
+  val errorMessage = stringResource(R.string.scan_error)
+  val reportError = stringResource(R.string.copy_error)
+  val errorCopied = stringResource(R.string.copied_to_clipboard)
+
   scanner
     .startScan()
     .addOnSuccessListener { onBarcodeDetected(it) }
-    .addOnCanceledListener { onCancel }
-    .addOnFailureListener { onCancel }
+    .addOnCanceledListener { onCancel() }
+    .addOnFailureListener { exception ->
+      SnackbarService.showSnackbar(
+        message = errorMessage,
+        actionLabel = reportError,
+        onAction = {
+          val clipboardManager =
+            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+          val clip =
+            ClipData.newPlainText(
+              "Barcode Scan Error",
+              "${exception.message}\n${Log.getStackTraceString(exception)}",
+            )
+          clipboardManager.setPrimaryClip(clip)
+
+          SnackbarService.showSnackbar(message = errorCopied)
+        },
+      )
+      onCancel()
+    }
 }
