c7f07c96328b342aa72efd9eee33aaac3987741d
pawcode Development <development@pawcode.de>
2025-05-18 15:56:13 +0200

    ci(release): rename APK file

--- release.config.cjs
+++ release.config.cjs
@@ -1,62 +1,69 @@
 const branch = process.env.GITHUB_REF_NAME;
 
-const assetsToUpdate = ['version.properties'];
-if (branch === 'master') {
-  assetsToUpdate.push('CHANGELOG.md');
+const assetsToUpdate = ["version.properties"];
+if (branch === "master") {
+  assetsToUpdate.push("CHANGELOG.md");
 }
 
 const config = {
-  branches: ['master', { name: 'staging', channel: 'next', prerelease: true }],
+  branches: ["master", { name: "staging", channel: "next", prerelease: true }],
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
+      "@semantic-release/commit-analyzer",
       {
-        preset: 'angular',
+        preset: "angular",
         releaseRules: [
-          { type: 'docs', scope: 'README', release: 'patch' },
-          { type: 'build', scope: 'deps', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'ci', release: 'patch' },
-          { type: 'i18n', release: 'patch' },
-          { type: 'style', release: 'patch' }
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "build", scope: "deps", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "i18n", release: "patch" },
+          { type: "style", release: "patch" }
         ],
         parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
         }
       }
     ],
     [
-      '@semantic-release/release-notes-generator',
+      "@semantic-release/release-notes-generator",
       {
-        preset: 'angular',
+        preset: "angular",
         parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
         },
         writerOpts: {
-          commitsSort: ['subject', 'scope']
+          commitsSort: ["subject", "scope"]
         }
       }
     ],
-    '@semantic-release/changelog',
+    "@semantic-release/changelog",
     [
-      '@semantic-release/exec',
+      "@semantic-release/exec",
       {
-        prepareCmd: './scripts/update-version.sh ${nextRelease.version}',
-        publishCmd: './scripts/build-apk.sh'
+        prepareCmd: "./scripts/update-version.sh ${nextRelease.version}",
+        publishCmd: "./scripts/build-apk.sh"
       }
     ],
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
         assets: assetsToUpdate
       }
     ],
     [
-        '@semantic-release/github',
-        {
-            assets: ['app/build/outputs/apk/release/*.apk'],
-            successCommentCondition: '<% return issue.pull_request || !nextRelease.channel || !issue.labels.some(label => label.name === "released on @next"); %>'
-        }
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "app/build/outputs/apk/release/*.apk",
+            name: "CardStore-${nextRelease.gitTag}.apk",
+            label: "CardStore (${nextRelease.version})"
+          }
+        ],
+        successCommentCondition:
+          '<% return issue.pull_request || !nextRelease.channel || !issue.labels.some(label => label.name === "released on @next"); %>'
+      }
     ]
   ]
 };
