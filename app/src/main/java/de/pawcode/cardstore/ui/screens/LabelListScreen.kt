064a6ccddc802284203c8c7a6a1d387614b712a4
pawcode Development <development@pawcode.de>
2025-05-18 19:56:40 +0200

    refactor(label-list-screen): update item layout

--- app/src/main/java/de/pawcode/cardstore/ui/screens/LabelListScreen.kt
+++ app/src/main/java/de/pawcode/cardstore/ui/screens/LabelListScreen.kt
@@ -1,6 +1,6 @@
 package de.pawcode.cardstore.ui.screens
 
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -34,7 +34,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -121,10 +120,7 @@ fun LabelListScreenComponent(
                   contentDescription = stringResource(R.string.labels_options),
                 )
               },
-              modifier =
-                Modifier.pointerInput(Unit) {
-                  detectTapGestures(onTap = { showLabelOptionSheet = label })
-                },
+              modifier = Modifier.padding(4.dp).clickable { showLabelOptionSheet = label },
             )
 
             HorizontalDivider()
