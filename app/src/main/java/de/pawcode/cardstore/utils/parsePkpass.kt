021922650718abd92bf2bfbb4fa47f161f41e79e
pawcode Development <development@pawcode.de>
2025-05-23 13:39:38 +0200

    refactor(pkpass): update card parser

--- app/src/main/java/de/pawcode/cardstore/utils/parsePkpass.kt
+++ app/src/main/java/de/pawcode/cardstore/utils/parsePkpass.kt
@@ -1,39 +1,37 @@
 package de.pawcode.cardstore.utils
 
-import android.graphics.Color
-import org.json.JSONObject
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+val RGB_REGEX = Regex("""rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)""")
 
 fun parsePkpass(passContent: String): Map<String, String?>? {
   if (passContent.isEmpty()) {
     return null
   }
-  val jsonObject = JSONObject(passContent)
 
-  val organizationName = jsonObject.getString("organizationName")
-  val content =
-    if (jsonObject.has("barcodes")) {
-      jsonObject.getJSONArray("barcodes").getJSONObject(0).getString("message")
-    } else {
-      jsonObject.getJSONObject("barcode").getString("message")
-    }
-
-  val backgroundColorRGB =
-    jsonObject
-      .getString("backgroundColor")
-      .replace("rgb(", "")
-      .replace(")", "")
-      .split(", ") // remove unnecessary information
-  val color =
-    Color.rgb(
-      backgroundColorRGB[0].toInt(),
-      backgroundColorRGB[1].toInt(),
-      backgroundColorRGB[2].toInt(),
-    )
+  val json = Json.parseToJsonElement(passContent).jsonObject
+  // extract store name
+  val description = json["description"]?.jsonPrimitive?.content
+  // extract card number
+  val barcode = json["barcodes"]?.jsonArray?.get(0)?.jsonObject ?: json["barcode"]?.jsonObject
+  val content = barcode?.get("message")?.jsonPrimitive?.content
+  // extract barcode format
+  val format = barcode?.get("format")?.jsonPrimitive?.content
+  val barcodeFormat = format?.let { mapPkpassBarcodeFormat(it) }
+  // extract color
+  val rgbString = json["backgroundColor"]?.jsonPrimitive?.content
+  val rgb = rgbString?.let { RGB_REGEX.find(it)?.groupValues?.drop(1)?.map { it.toInt() } }
+  val color = rgb?.let { Color(it[0], it[1], it[2]) }
 
   return mapOf(
-    "storeName" to organizationName,
+    "storeName" to description,
     "cardNumber" to content,
-    "barcodeFormat" to "QR_CODE",
-    "color" to color.toString(),
+    "barcodeFormat" to barcodeFormat.toString(),
+    "color" to color?.toArgb().toString(),
   )
 }
