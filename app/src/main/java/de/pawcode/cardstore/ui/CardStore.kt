cb94b81b4b9c2777a5d9d9cd42367c1b84c8962a
pawcode Development <development@pawcode.de>
2025-11-23 11:56:19 +0100

    refactor(navigation): replace NavGraph with entryProvider
    This is step 5 of the navigation3 migration

--- app/src/main/java/de/pawcode/cardstore/ui/CardStore.kt
+++ app/src/main/java/de/pawcode/cardstore/ui/CardStore.kt
@@ -3,9 +3,19 @@ package de.pawcode.cardstore.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.entryProvider
 import de.pawcode.cardstore.navigation.Navigator
+import de.pawcode.cardstore.navigation.ScreenAbout
+import de.pawcode.cardstore.navigation.ScreenCardEdit
 import de.pawcode.cardstore.navigation.ScreenCardList
+import de.pawcode.cardstore.navigation.ScreenLabelEdit
+import de.pawcode.cardstore.navigation.ScreenLabelList
 import de.pawcode.cardstore.navigation.rememberNavigationState
+import de.pawcode.cardstore.ui.screens.AboutScreen
+import de.pawcode.cardstore.ui.screens.CardListScreen
+import de.pawcode.cardstore.ui.screens.EditCardScreen
+import de.pawcode.cardstore.ui.screens.EditLabelScreen
+import de.pawcode.cardstore.ui.screens.LabelListScreen
 
 @Composable
 fun CardStore(modifier: Modifier = Modifier) {
@@ -13,4 +23,21 @@ fun CardStore(modifier: Modifier = Modifier) {
     rememberNavigationState(startRoute = ScreenCardList, topLevelRoutes = setOf(ScreenCardList))
 
   val navigator = remember { Navigator(navigationState) }
+
+  entryProvider {
+    entry<ScreenCardList> { CardListScreen(navigator) }
+    entry<ScreenCardEdit> { data ->
+      EditCardScreen(
+        navigator,
+        data.cardId,
+        data.storeName,
+        data.cardNumber,
+        data.barcodeType,
+        data.color,
+      )
+    }
+    entry<ScreenLabelList> { LabelListScreen(navigator) }
+    entry<ScreenLabelEdit> { data -> EditLabelScreen(navigator, data.labelId) }
+    entry<ScreenAbout> { AboutScreen(navigator) }
+  }
 }
